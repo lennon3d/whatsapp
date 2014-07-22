6f66eb66c6de7fe45d80622602906a0c79b09ef9
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2014-07-22 20:54:43 +0530

    config changed

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ session_start();
 // Valid constant names
 define("HOST",     	"localhost");
 define("DB_USER",  	"root");
-define("DB_PASS", 	"");
+define("DB_PASS", 	"root");
 define("DB_NAME", 	"whatsapp");
 //Whats APP Config
 ?>
